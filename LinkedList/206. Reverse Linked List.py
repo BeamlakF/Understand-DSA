ec7d192b28bef2b05e58e92991d0cd16c4b26c81
BeamlakF <beamlakfekadu562@gmail.com>
2025-09-20 04:26:40 +0300

    Understand LinkedLists

--- LinkedList/206. Reverse Linked List.py
+++ LinkedList/206. Reverse Linked List.py
@@ -4,10 +4,10 @@ class Solution:
         curr = head
 
         while curr:
-            next_temp = curr.next
-            curr.next = prev
-            prev =curr
-            curr = next_temp
+            next_temp = curr.next # this is to save the continuing List from being lost other wise, curr.next will have nothing to point at,
+            curr.next = prev # this is the reversal point
+            prev =curr # save the reverses node in to the current one
+            curr = next_temp # update the saved list
 
         return prev
 
