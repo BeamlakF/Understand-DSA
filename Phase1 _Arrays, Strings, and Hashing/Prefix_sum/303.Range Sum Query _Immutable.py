29aab147cb8637db618fd2c2f7c2202583ff6cf8
BeamlakF <beamlakfekadu562@gmail.com>
2025-10-28 22:30:47 +0300

    303.Range Sum Query _Immutable---prefix sum question

--- Phase1 _Arrays, Strings, and Hashing/Prefix_sum/303.Range Sum Query _Immutable.py
+++ Phase1 _Arrays, Strings, and Hashing/Prefix_sum/303.Range Sum Query _Immutable.py
@@ -11,3 +11,18 @@ class NumArray:
         return total
 
 # The above is Brute force approach and results in time limit exceeded for large inputs on Leetcode
+
+
+class NumArray:
+    def __init__(self, nums):
+        # build prefix sum array
+        self.prefix = [0]
+        for num in nums:
+            self.prefix.append(self.prefix[-1] + num)
+
+    def sumRange(self, left, right):
+        # get range sum in O(1)
+        return self.prefix[right + 1] - self.prefix[left]
+    
+    
+# The above is Optimal approach using Prefix sum technique
